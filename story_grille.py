6bdff46187cb0db17e9d994e597dcd299831da35
naimisalima2-cmd <naimisalima2@gmail.com>
2025-09-29 10:15:52 +0100

    User Story 'Plouf dans l'eau' ajoutée avec lignes commentées

---
+++ story_grille.py
@@ -0,0 +1,8 @@
+# story_grille.py
+# from grille import Grille 
+# g = Grille(5, 8)
+# while True:
+#     # g.afficher()
+#     # x = int(input("Entrez la ligne : "))
+#     # y = int(input("Entrez la colonne : "))
+#     # g.tirer(x, y)
