0fcf97033d2fd5d080e53efdd0c088664eecebfa
naimisalima2-cmd <naimisalima2@gmail.com>
2025-10-05 12:30:09 +0100

    Ajout de la méthode Grille.ajoute pour placer un bateau et tests de placement

--- grille.py
+++ grille.py
@@ -1,3 +1,4 @@
+from bateau import Bateau
 class Grille:
     def __init__(self,nombre_lignes,nombre_colonnes):
         self.nombre_colonnes=nombre_colonnes
@@ -12,13 +13,19 @@ class Grille:
             for j in range(self.nombre_colonnes):
                 index=i*self.nombre_colonnes+j
                 print(self.matrice[index],end='')
+            print()
     def tirer(self, ligne, colonne):
         if 0 <= ligne < self.nombre_lignes and 0 <= colonne < self.nombre_colonnes:
-            self.matrice[ligne][colonne] = self.touche
+            index = ligne * self.nombre_colonnes + colonne
+            self.matrice[index] = self.touche
         else:
             print("Coordonnées invalides.")
+    def ajoute(self, bateau: Bateau):
+        for (ligne, col) in bateau.positions:
+            if ligne >= self.nombre_lignes or col >= self.nombre_colonnes:
+                return "Grille inchangée"  
+        for (ligne, col) in bateau.positions:
+            index = ligne * self.nombre_colonnes + col
+            self.matrice[index] = '⛵'
+        return "Bateau placé avec succès"
 
-            
-
-
-        
--- test_grille.py
+++ test_grille.py
@@ -1,8 +1,26 @@
 from grille import Grille
+from bateau import Bateau
+
 g = Grille(5, 8)
 print("Grille initiale :")
 g.afficher()
+
 print("\nAprès le tir à la case (2,3) :")
 g.tirer(2, 3)
 g.afficher()
+
+g = Grille(2, 3)
+print("\nAjout bateau horizontal qui rentre :")
+print(g.ajoute(Bateau(1, 0, longueur=2, vertical=False)))
+g.afficher()
+
+g2 = Grille(2, 3)
+print("\nAjout bateau vertical qui ne rentre pas :")
+print(g2.ajoute(Bateau(1, 0, longueur=2, vertical=True)))
+g2.afficher()
+
+g3 = Grille(2, 3)
+print("\nAjout bateau vertical trop grand :")
+print(g3.ajoute(Bateau(1, 0, longueur=4, vertical=True)))
+g3.afficher()
 
